97485cf9a1aad374a167421e6f500c4584d926ac
Davidson <davidsonalves47@gmail.com>
2023-09-12 21:52:15 -0300

    Aula finalizada tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class TiposVariaveis {
+/*public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Mundo!");
+    }
+}*/
+
+
+public class TiposVariaveis {
+    public static void main (String[] args) throws Exception {
+        double salariMinimo = 2500.44;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double valor_de_pi = 3.14;
     }
 }
