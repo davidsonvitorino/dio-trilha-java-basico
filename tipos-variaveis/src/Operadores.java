fbc557a75c79e93b84d182f64ebc6d7bb7d75bc3
Davidson <davidsonalves47@gmail.com>
2023-09-13 06:10:40 -0300

    Aula de Concatenação de strings e numeros

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Concatenação
+        String nomeCompleto = "Linguadem " + "Java";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 2+3+2+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" +(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
