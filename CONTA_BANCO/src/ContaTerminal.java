0d17175ba6d8947ad71f13b0b91651e0584b4bca
Davidson <davidsonalves47@gmail.com>
2023-09-25 17:12:00 -0300

    Desafio "ContaTerminal"

--- CONTA_BANCO/src/ContaTerminal.java
+++ CONTA_BANCO/src/ContaTerminal.java
@@ -1,28 +1,42 @@
 import java.util.Scanner;
 
-public class ContaTerminal {
-    public static void main (String[] args) {
-        //objeto scanner
-        Scanner scanner = new Scanner(System.in);
-        var banco = "Caixa Economica";
 
-        System.out.print("Digite o codigo do banco: ");
-        int codigo = scanner.nextInt();
-        if(codigo == 104){
-            banco = "Caixa Economica Federal";
-        }
-        else if(codigo == 105){
-            banco = "Bradesco";
-        }
-        else if(codigo == 106){
-            banco = "Itaú";
-        }else {
-            banco = "Codigo invalido";
-        }
-        System.out.println("Banco: " + banco);
 
-        System.out.print("Digite a sua agência: ");
-        int agencia = scanner.nextInt();
+public class ContaTerminal{
+    private String numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaTerminal(String numero, String agencia, String nomeCliente, double saldo){
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
     }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo!");
+
+        System.out.print("Digite o número da conta: ");
+        String numeroConta = scanner.nextLine();
+
+        System.out.print("Digite a agência da conta: ");
+        String agenciaConta = scanner.nextLine();
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o saldo inicial da conta: ");
+        double saldoInicial = scanner.nextDouble();
+
+        ContaTerminal conta = new ContaTerminal(numeroConta, agenciaConta, nomeCliente, saldoInicial);
+    }
+    
 }
